c4ecbf2fc7f501b4550b9aa061843894f02904fb
Akilesh Praveen <akilesh.praveen@gmail.com>
2018-07-19 03:13:50 +0100

    add all files

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal();
+            var headerReveal = {
+                delay:200,
+                distance:'90px',
+                vFactor : 0.6
+                
+            };
+            sr.reveal('.animated', headerReveal);
+            $('.myNav').tooltip()
+
+
+
+$(document).ready(function(){
+  var animTime = 300,
+      clickPolice = false;
+  
+  $(document).on('touchstart click', '.acc-btn', function(){
+    if(!clickPolice){
+       clickPolice = true;
+      
+      var currIndex = $(this).index('.acc-btn'),
+          targetHeight = $('.acc-content-inner').eq(currIndex).outerHeight();
+   
+      $('.acc-btn h1').removeClass('selected');
+      $(this).find('h1').addClass('selected');
+      
+      $('.acc-content').stop().animate({ height: 0 }, animTime);
+      $('.acc-content').eq(currIndex).stop().animate({ height: targetHeight }, animTime);
+
+      setTimeout(function(){ clickPolice = false; }, animTime);
+    }
+    
+  });
+  
+});
